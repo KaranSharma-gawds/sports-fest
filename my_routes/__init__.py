ca7904d1b35f61ed8985c23689e5b998fe3e8916
karan sharma <karansharma232ds@gmail.com>
2018-01-03 13:56:37 +0530

    fest route added

--- my_routes/__init__.py
+++ my_routes/__init__.py
@@ -5,12 +5,14 @@ login_manager = LoginManager()
 institute = Blueprint('institute', __name__)
 login = Blueprint('login', __name__)
 people = Blueprint('people', __name__)
+sports_fest = Blueprint('sports_fest', __name__)
 from . import institution
 from . import user
 from . import person
-
+from . import fest
 def initialise_routes(app):
     login_manager.init_app(app)
     app.register_blueprint(institute, url_prefix='/institute')
     app.register_blueprint(login, url_prefix='/user')
     app.register_blueprint(people, url_prefix='/people')
+    app.register_blueprint(sports_fest, url_prefix='/fest')
