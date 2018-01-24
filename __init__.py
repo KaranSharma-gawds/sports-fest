8664699c7163ece2b42c8e18ceef539bfb03226b
karan sharma <karansharma232ds@gmail.com>
2018-01-24 22:06:41 +0530

    index ke liye route daal diya

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import config as config
 from flask_api import FlaskAPI
+from flask import render_template
 from my_routes import initialise_routes
 
 app = FlaskAPI(__name__)
@@ -8,6 +9,10 @@ app = FlaskAPI(__name__)
 def run():
     initialise_routes(app)
     app.run(host=config.HOST, port=config.PORT, debug=True, threaded=False, processes=1)
+
+@app.route('/', methods=['GET'])
+def indexRoute():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     run()
