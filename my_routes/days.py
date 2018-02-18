e6b4f8c0f04e1dbeedc856ff01614a4cf163b357
karan sharma <karansharma232ds@gmail.com>
2018-02-18 23:36:28 +0530

    retrieving from front-end branch

--- my_routes/days.py
+++ my_routes/days.py
@@ -2,6 +2,7 @@ from flask import request, redirect
 from flask_login import login_required
 from models import Event, Day
 from connection import DatabaseHandler
+from .upload_file import upload_file
 from . import day
 
 session = DatabaseHandler.connect_to_database()
@@ -54,4 +55,30 @@ def add_blank_day(event_id):
     # return redirect('/dashboard')
     return {
         'status':'OK'
+    }
+@day.route('/upload/result/<int:day_id>', methods=['POST', 'GET'])
+def upload_result(day_id):
+    if request.method == 'GET':
+        return render_template('upload_day_result.html', url=url, filetype='doc')
+    if 'doc' not in request.files:
+        # flash('no doc part in request')
+        print('no doc part in request')
+        return redirect(request.url)
+    photo = request.files['doc']
+    obj = upload_file(photo, 'DOC')
+    if obj['status'] == 'BAD REQUEST':
+        print(obj['message'])
+        return obj
+    if obj['status'] == 'OK':
+        print(obj['message'])
+        filename = obj['filename']
+        # info = UploadedFile(filename=filename, file_type='DOC')
+        my_day = Day.query.filter_by(id=day_id).first()
+        my_day.result_pdf = filename
+        session.add(my_day)
+        session.commit()
+        return redirect('/dashboard')
+    return {
+        'status':'ERROR',
+        'message':'UNPREDICTED ERROR OCCURRED'
     }
